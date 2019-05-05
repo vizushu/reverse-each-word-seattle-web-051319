3b8c36681746b580741e2b88af73155ab2907c6e
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-05 04:04:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,7 @@
 def reverse_each_word(a)
   a = "olleH ,ereht dna woh era ?uoy"
   a.reverse {|x| print x, " " }
+  def reverse_each_word(sentence)
+ sentence.reverse.split.reverse.join(" ")
+end
 end
